d41b6d6c8e51ce96742f7b1c208f16fff62c55eb
Kojo Appiah <Kojo.E.Appiah@gmail.com>
2018-02-19 14:23:42 -0500

    Refactoring Pot Constructor

--- pot.js
+++ pot.js
@@ -1,10 +1,40 @@
+const Marble = require('./Marble.js');
+
 class Pot {
-	constructor(){
-		this.marbleCount = 0;
+	constructor(numberOfMarbles){
+		this.marbles = this.generateMarbles(numberOfMarbles);
+		this.length = 20;
+		this.height = 20;
+	}
+
+	getLength(){
+		return this.length;
+	}
+
+	getHeight(){
+		return this.height;
 	}
 
 	getMarbleCount(){
-		return this.marbleCount;
+		return this.marbles.length;
+	}
+
+	getMarbles(){
+		return this.marbles;
+	}
+
+	addMarble(marbleToAdd){
+		this.marbles.push(marbleToAdd);
+
+	}
+
+	generateMarbles(numberOfMarbles){
+		var newMarbles = [];
+		for(var i = 0; i < numberOfMarbles; i++){
+			newMarbles.push(new Marble());
+		}
+
+		return newMarbles;
 	}
 }
 
--- pot.test.js
+++ pot.test.js
@@ -1,11 +1,28 @@
 const Pot = require('./pot');
+const Marble = require('./marble');
 
 var pot;
 
 beforeEach(()=> { 
-	pot = new Pot();
+	pot = new Pot(5);
 });
 
-test('Checks that pot starts with 0 marbles', () => {
-  expect(pot.getMarbleCount()).toEqual(0);
+test('Checks that pot starts with a length of 20', () => {
+    expect(pot.getLength()).toEqual(20);
+});
+
+test('Checks that pot starts with a height of 20', () => {
+    expect(pot.getHeight()).toEqual(20);
+});
+
+test('Checks that pot initialized with 5 marbles starts with marblecount of 5', () => {
+    expect(pot.getMarbleCount()).toEqual(5);
+});
+
+test('When a marble is added to a non-empty pot, said pot retains said marble', () => {
+    marble1 = new Marble();
+    pot.addMarble(marble1);
+    marble2 = new Marble();
+    pot.addMarble(marble2);
+    expect(pot.getMarbles().length).toEqual(7);
 });
